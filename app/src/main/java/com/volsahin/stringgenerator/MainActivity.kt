0a66eb1e9cf05568e2f3d35334005ad9b009ac43
volsahin <cesahinvolkan@gmail.com>
2020-10-19 01:00:38 +0300

    Gradle plugin lines commented out since after pull project there are no local plugins has published yet.

--- app/src/main/java/com/volsahin/stringgenerator/MainActivity.kt
+++ app/src/main/java/com/volsahin/stringgenerator/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appName = getString(R.string.app_name)
+        // val description = RR.string.description
     }
 }
